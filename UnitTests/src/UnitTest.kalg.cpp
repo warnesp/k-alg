d91366850a2f94cb9f2d9031338f561250eb4cd5
Paul Warnes <pwarnes@gmail.com>
2022-10-11 19:11:16 -0500

    use sections in unit test

--- UnitTests/src/UnitTest.kalg.cpp
+++ UnitTests/src/UnitTest.kalg.cpp
@@ -44,43 +44,37 @@ TEST_CASE("sort", "[kalg]") {
     REQUIRE(g.addEdge(4, 1));
     REQUIRE(g.addEdge(2, 3));
 
-    auto result = g.sort();
+    SECTION("normal sort") {
+        auto result = g.sort();
 
-    REQUIRE(result.size() == g.size());
+        REQUIRE(result.size() == g.size());
 
-    REQUIRE(result[0] == 0);
-    REQUIRE(result[1] == 4);
-    REQUIRE(result[2] == 5);
-    REQUIRE(result[3] == 2);
-    REQUIRE(result[4] == 3);
-    REQUIRE(result[5] == 1);
+        REQUIRE(result[0] == 0);
+        REQUIRE(result[1] == 4);
+        REQUIRE(result[2] == 5);
+        REQUIRE(result[3] == 2);
+        REQUIRE(result[4] == 3);
+        REQUIRE(result[5] == 1);
+    }
+
+    SECTION("sort with loop") {
+
+
+        REQUIRE(g.addEdge(1, 2));
+
+        auto result = g.sort();
+
+        // make sure results don't include the loop
+        REQUIRE(result.size() != g.size());
+        REQUIRE(result.size() == 3);
+        REQUIRE(g.size() == GRAPH_SIZE);
+
+        // only nodes without loop
+        REQUIRE(result[0] == 0);
+        REQUIRE(result[1] == 4);
+        REQUIRE(result[2] == 5);
+    }
 }
 
-
-TEST_CASE("sort-loop", "[kalg]") {
-
-
-    constexpr int GRAPH_SIZE = 6;
-    Graph g(GRAPH_SIZE);
-
-    // add some nodes with a loop
-    REQUIRE(g.addEdge(4, 2));
-    REQUIRE(g.addEdge(3, 1));
-    REQUIRE(g.addEdge(4, 3));
-    REQUIRE(g.addEdge(2, 3));
-    REQUIRE(g.addEdge(1, 4));
-
-    auto result = g.sort();
-
-    // make sure results don't include the loop
-    REQUIRE(result.size() != g.size());
-    REQUIRE(result.size() == 2);
-    REQUIRE(g.size() == GRAPH_SIZE);
-
-    // only nodes without loop
-    REQUIRE(result[0] == 0);
-    REQUIRE(result[1] == 5);
-
-}
 
 
